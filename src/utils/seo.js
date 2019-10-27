82f4a40a32c74ad385ffd7657480c06dab61c8d8
JuhG <juhg24@gmail.com>
2019-10-27 12:42:18 +0100

    wip

--- src/utils/seo.js
+++ src/utils/seo.js
@@ -1,13 +1,13 @@
 import React from "react"
 import { Helmet } from "react-helmet";
 import { graphql, useStaticQuery } from "gatsby"
-import { RichText } from 'prismic-reactjs'
 
 export default ({
     children,
     title,
     description,
     titleTemplate = null,
+    image
 }) => {
     const { site } = useStaticQuery(graphql`
         {
@@ -25,17 +25,26 @@ export default ({
     const metaDescription = description || (site && site.siteMetadata.description)
 
     return (
-        <Helmet
+        <Helmet defer={false}
             titleTemplate={titleTemplate || `%s | ${site && site.siteMetadata.title}`}
             defaultTitle={`${site && site.siteMetadata.title}`}
         >
             <html lang={site && site.siteMetadata.language} />
-            <title>{title}</title>
+            <meta
+                name="viewport"
+                content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
+            />
 
+            <title>{title}</title>
             <meta name="description" content={metaDescription} />
+
+            <meta property="og:locale" content={site && site.siteMetadata.language} />
+            <meta property="og:site_name" content={site && site.siteMetadata.title} />
             <meta property="og:title" content={title} />
             <meta property="og:description" content={metaDescription} />
             <meta property="og:type" content="website" />
+            <meta property="og:image" content={image} />
+
             <meta name="twitter:card" content="summary" />
             <meta name="twitter:creator" content={site && site.siteMetadata.author} />
             <meta name="twitter:title" content={title} />
