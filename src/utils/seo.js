329f17edf1323503051d491b72133ee6b990574d
JuhG <juhg24@gmail.com>
2019-12-03 12:58:34 +0100

    better seo titles

--- src/utils/seo.js
+++ src/utils/seo.js
@@ -2,13 +2,7 @@ import React from "react";
 import { Helmet } from "react-helmet";
 import { graphql, useStaticQuery } from "gatsby";
 
-export default ({
-  children,
-  title,
-  description,
-  titleTemplate = null,
-  image
-}) => {
+export default ({ children, title, description, url, noTemplate, image }) => {
   const { site, home } = useStaticQuery(graphql`
     {
       site {
@@ -40,32 +34,34 @@ export default ({
       : site.siteMetadata;
 
   const metaDescription = description || data.description;
+  const metaTitle = title
+    ? noTemplate
+      ? title
+      : `${title} | ${data.title}`
+    : data.title;
 
   return (
-    <Helmet
-      defer={false}
-      titleTemplate={titleTemplate || `%s | ${data.title}`}
-      defaultTitle={`${data.title}`}
-    >
+    <Helmet defer={false}>
       <html lang={data.language} />
       <meta
         name="viewport"
         content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
       />
 
-      <title>{title}</title>
+      <title>{metaTitle}</title>
       <meta name="description" content={metaDescription} />
 
       <meta property="og:locale" content={data.language} />
       <meta property="og:site_name" content={data.title} />
-      <meta property="og:title" content={title} />
+      <meta property="og:title" content={metaTitle} />
       <meta property="og:description" content={metaDescription} />
       <meta property="og:type" content="website" />
       <meta property="og:image" content={image || home.data.image.url} />
+      <meta property="og:url" content={url} />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:creator" content={data.author} />
-      <meta name="twitter:title" content={title} />
+      <meta name="twitter:title" content={metaTitle} />
       <meta name="twitter:description" content={metaDescription} />
 
       {children}
